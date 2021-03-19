4f8ed8a491f9e4a76824ee6e38eadaf13ef076da
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2021-03-20 00:47:02 +0530

    Read Comments Carefully
    All the editing should be done as per the content mentioned in the comments for easy running of the desired process.

--- Get_IP/IP.bat
+++ Get_IP/IP.bat
@@ -1,6 +1,11 @@
 @echo off
 
 :loop
+
+:: You should use "Wi-Fi" rather than "Network Bridge" if you have your Wi-Fi adaptor as public 
+:: Network Bridge - Bridged connection (Bridge Metered or Private Network)
+:: Wi-Fi - Public Network (No Bridge, only OPEN or Public)
+
 for /f "tokens=2 delims=:" %%g in ('netsh interface ip show address "Network Bridge" ^| findstr "Default"') do set DefaultGateway_1=%%g
 echo %DefaultGateway_1%
 echo %DefaultGateway_1% > IP-1.txt
